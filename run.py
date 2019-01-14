bd9383458e4ea5e024a9cad411a54dfb7afb3f1c
Susanne Fortunato <susannefortunato@Susannes-MacBook-Air.local>
2019-01-14 16:47:10 -0500

    nothing working right now

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+# from xml import xpath
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options 
 
-from .credentials import username, password
+from credentials import username, password
 
 
 def get_profile():
     profile = FirefoxProfile()
     # Disable CSS
-    profile.set_preference('permissions.default.stylesheet', 2)
+    # profile.set_preference('permissions.default.stylesheet', 2)
     # Disable images
-    profile.set_preference('permission.default.image', 2)
+    # profile.set_preference('permission.default.image', 2)
     # Disable Flash
     profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so','false')
 
@@ -21,21 +22,28 @@ def run():
     profile = get_profile()
     options = Options()
     options.add_argument("--headless")
-    driver = webdriver.Firefox(profile)
+    driver = webdriver.Firefox(firefox_profile=profile, options=options)
     driver.implicitly_wait(30)
 
-    link = "https://www.fabletics.com/"
+    link = "https://www.fabletics.com/login"
     driver.get(link)
     driver.implicitly_wait(30)
 
-    results = {
-        'url': driver.current_url,
-        'source': driver.page_source
+    # Enter username
+    driver.find_element_by_id("reference_data").send_keys(username)
+    # Enter password
+    driver.find_element_by_id("verification_data").send_keys(password)
+    # Click login button
+    driver.findElement(By.linkText("Sign In"))
+    driver.implicitly_wait(30)
 
-    }
+    html = driver.page_source()
+    if "Susanne" in html:
+        print("logged in successfully")
+    else:
+        print("try again")
 
     driver.quit()
-    return results
 
 if __name__ == '__main__':
     run()
@@ -44,3 +52,12 @@ if __name__ == '__main__':
 
 # driver.find_element_by_id("name of id").click()
 # driver.get("url")
+
+# Button html
+# <button class="button" data-role="none" type="submit">Sign In</button>
+# driver.findElement(By.xpath("//button[text()='Confirm']")).click();
+# driver.find_elements_by_xpath("//*[contains(text(), 'My Button')]")
+
+# driver.findElement(By.linkText("Log in")):
+
+

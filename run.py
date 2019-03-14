a6ab63ed300837e66b01abbba941b489d79146a6
Susanne Fortunato <susannefortunato@Susannes-MacBook-Air.local>
2019-03-14 16:47:24 -0400

    can log in, clear the modal safely. need to wait until EOM to implement skip_month() workflow

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # from xml import xpath
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options 
@@ -7,24 +8,24 @@ from selenium.webdriver.firefox.options import Options
 from credentials import username, password
 
 
-def get_profile():
-    profile = FirefoxProfile()
-    # Disable CSS
-    # profile.set_preference('permissions.default.stylesheet', 2)
-    # Disable images
-    # profile.set_preference('permission.default.image', 2)
-    # Disable Flash
-    profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so','false')
 
-    return profile
+def safe_clear_modal(driver):
+    print("in safe_clear_modal")
+    try:
+        print("in try")
+        modal_x = driver.find_element_by_id("cboxClose")
+        modal_x.click()
+        return True
+    except:
+        print('in except')
+        return False 
 
-def run():
-    profile = get_profile()
-    options = Options()
-    # options.add_argument("--headless")
-    driver = webdriver.Firefox(firefox_profile=profile, options=options)
-    driver.implicitly_wait(30)
 
+def skip_month(driver):
+    pass
+
+
+def log_in(driver):
     link = "https://www.fabletics.com/index.cfm?action=home.login"
     driver.get(link)
     driver.implicitly_wait(30)
@@ -38,36 +39,43 @@ def run():
     driver.find_element_by_id("submitSignIn").click()
     driver.implicitly_wait(30)
 
-    html = driver.page_source()
-    if "Susanne" in html:
-        print("logged in successfully")
-    else:
-        print("try again")
+    return driver
+
+
+def get_profile():
+    profile = FirefoxProfile()
+    # Disable CSS
+    # profile.set_preference('permissions.default.stylesheet', 2)
+    # Disable images
+    # profile.set_preference('permission.default.image', 2)
+    # Disable Flash
+    profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so','false')
+
+    return profile
+
+def get_webdriver():
+    profile = get_profile()
+    options = Options()
+    # options.add_argument("--headless")
+    driver = webdriver.Firefox(firefox_profile=profile, options=options)
+    driver.implicitly_wait(30)
+
+    return driver
+
+
+def run():
+
+    driver = get_webdriver()
+
+    log_in(driver)
+    time.sleep(4)
+    safe_clear_modal(driver)
+    time.sleep(4)
+    skip_month(driver)
 
     driver.quit()
 
 if __name__ == '__main__':
     run()
 
-
-
-# driver.find_element_by_id("name of id").click()
-# driver.get("url")
-
-# Button html
-# <button class="button" data-role="none" type="submit">Sign In</button>
-# driver.findElement(By.xpath("//button[text()='Confirm']")).click();
-# driver.find_elements_by_xpath("//*[contains(text(), 'My Button')]")
-
-# driver.findElement(By.linkText("Log in")):
-
-'''
-Modal: button to x-out of modal id = 'cboxClose' --> may vary significantlly
-'sign in' find by link text 'Sign In'
-alt: just go straight to https://www.fabletics.com/index.cfm?action=home.login
-
-Alternate URL to login login, skip modal: https://www.fabletics.com/index.cfm?action=home.login 
-
-
-'''
 
